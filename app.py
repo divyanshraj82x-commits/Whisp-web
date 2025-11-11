6e82550653d3a1af04a94ee67f7b2ba1372b3dc4
divyanshraj82x-commits <divyanshraj82x@gmail.com>
2025-11-11 16:07:25 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@socketio.on('message')
+def handle_message(msg):
+    print(f"Message: {msg}")
+    send(msg, broadcast=True)
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', debug=True)
