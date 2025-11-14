b5385752bbcda225f494ab694918ca4b39ddd3b0
divyanshraj82x-commits <divyanshraj82x@gmail.com>
2025-11-14 15:21:45 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -17,4 +17,5 @@ def handle_message(msg):
     send(msg, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', debug=True)
+    socketio.run(app, host='0.0.0.0', port=10000, debug=False, allow_unsafe_werkzeug=True)
+
